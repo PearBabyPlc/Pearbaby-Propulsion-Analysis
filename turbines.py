63c9873dbc012aa8865eb895ac01e5663e412ee2
PearBaby <38026571+PearBabyPlc@users.noreply.github.com>
2025-12-22 02:22:37 +1100

    Update and rename formulaegg.py to turbines.py

--- turbines.py
+++ turbines.py
@@ -2,3 +2,6 @@ import math
 
 def turbineTW(eta, Cp, Ttin, Ptin, Ptout, gamma):
     return eta * Cp * Ttin * (1 - (Ptout / Ptin)**((gamma - 1) / gamma))
+
+#TODO reaction turbine specific speed stuff, probably a lookup function
+#also the beginnings of defining geometry and performance
